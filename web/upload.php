ff502ffa123d87a4e6cc220a11c9c2b1fdc628e1
Unknown <jasv61@gmail.com>
2018-01-18 09:47:17 +0100

    Réorganisation de upload.php

--- web/upload.php
+++ web/upload.php
@@ -1,48 +1,65 @@
 <?php
+
 if (isset($_FILES["fileToUpload"]) && $_FILES["fileToUpload"] != ""){
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "Le fichier est une image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "Le fichier n'est pas une image.";
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    // Check if image file is a actual image or fake image
+
+    if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+
+        if($check !== false) {
+            echo "Le fichier est une image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } 
+
+        else {
+            echo "Le fichier n'est pas une image.";
+            $uploadOk = 0;
+        }
+    }
+
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Désolé, votre fichier existe déjà.";
         $uploadOk = 0;
     }
-}
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Désolé, votre fichier existe déjà.";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Désolé, votre fichier est trop volumineux.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Désolé, seulement les fichiers JPG, JPEG, PNG & GIF sont autorisé.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Désolé, votre fichier n'a pas été charger.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " telechargement réussi.";
-    } else {
-        echo "Désolé, une erreur s'est produite lors du téléchargement de votre fichier.";
+
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Désolé, votre fichier est trop volumineux.";
+        $uploadOk = 0;
+    }
+
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+        echo "Désolé, seulement les fichiers JPG, JPEG, PNG & GIF sont autorisé.";
+        $uploadOk = 0;
+    }
+
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Désolé, votre fichier n'a pas été charger.";
+        // if everything is ok, try to upload file
+    } 
+
+    else {
+
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " telechargement réussi.";
+        }
+
+        else {
+            echo "Désolé, une erreur s'est produite lors du téléchargement de votre fichier.";
+        }
     }
 }
-}
+
+//Test de la condition
+
 // else {
 //     echo "sa bug";
 // }
