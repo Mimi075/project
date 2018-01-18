0da61208fb86f8e4d9102fb185ce080eda609c05
Unknown <jasv61@gmail.com>
2018-01-18 09:33:58 +0100

    Réorganisation controller

--- src/controllers.php
+++ src/controllers.php
@@ -11,12 +11,14 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 include '../web/function.php';
 //----------------------------------------------------------------------------------------------------
 $app->get('/', function () use ($app) {
+
     return $app['twig']->render('index.html.twig', regionList());
 })
 ->bind('homepage')
 ;
 //----------------------------------------------------------------------------------------------------
 $app->get('/login', function (Request $request) use ($app) {
+
     $username = $request->server->get('PHP_AUTH_USER', false);
     $password = $request->server->get('PHP_AUTH_PW');
 
@@ -28,26 +30,32 @@ $app->get('/login', function (Request $request) use ($app) {
     $response = new Response();
     $response->headers->set('WWW-Authenticate', sprintf('Basic realm="%s"', 'site_login'));
     $response->setStatusCode(401, 'Please sign in.');
+
     return $response;
 })
-->bind('login');
+->bind('login')
+;
 //----------------------------------------------------------------------------------------------------
 $app->get('/logout', function (Request $request) use ($app) {
     
     $request = $this->getRequest();
-   $session = $request->getSession();
-   $session->remove();
+    $session = $request->getSession();
+    $session->remove();
+    
     return $app->redirect('/');
 })
 ->bind('logout');
 //----------------------------------------------------------------------------------------------------
 $app->match('/inscription', function () use ($app) {
+    
     $dataInscription = array();
+    
     if (!empty($_POST)){
         foreach($_POST as $key => $value){
             //stockage dans un tableau des valeur reçu par $_POST avec neutralisation
             $post[$key] = $value;
         }
+        
         //initialisation du tableau des erreurs
         $errors = array();
 
@@ -55,30 +63,39 @@ $app->match('/inscription', function () use ($app) {
         if(strlen($post['firstname']) < 3){
             $errors[] = 'Le prénom doit comporter au moins 3 caractères';
         }
+        
         if(strlen($post['lastname']) < 3){
             $errors[] = 'Le nom doit comporter au moins 3 caractères';
         }
+        
         if(strlen($post['adress']) < 3){
             $errors[] = 'L\'adresse doit comporter au moins 3 caractères';
         }
+        
         if(strlen($post['zip']) != 5 ){
             $errors[] = 'Le code postale doit comporter 5 chiffres';
         }
+        
         if(empty($post['city'])){
             $errors[] = 'La ville ne peut être vide';
         }
+        
         if(strlen($post['siren']) != 9 ){
             $errors[] = 'Le n° siren doit comporter 9 chiffres';
         }
+        
         if(!filter_var($post['email'], FILTER_VALIDATE_EMAIL)){
             $errors[] = 'L\'adresse email est invalide';
         }
+        
          if(strlen($post['password']) < 6 ){
             $errors[] = 'Le mot de passe doit comporter au moins 6 caractères';
         }
+        
          if($post['password'] != $post['confPassword']){
             $errors[] = 'Les mot de passe ne sont pas identique';
         }
+        
         if(count($errors) == 0){
             // errors = 0 = insertion user
             $eleveur = new Entity\Eleveur();
@@ -93,13 +110,14 @@ $app->match('/inscription', function () use ($app) {
             $app['em']->persist($eleveur);
             $app['em']->flush();
         }
+
         if(!empty($errors)){
                    $dataInscription = [
                         'errors' => $errors
                    ];
         }
-   
     }
+
     return $app['twig']->render('inscription.html.twig', $dataInscription);
 })
 ->bind('inscription')
@@ -107,6 +125,7 @@ $app->match('/inscription', function () use ($app) {
 ;
 //----------------------------------------------------------------------------------------------------
 $app->match('/formulaireContact', function () use ($app) {
+
     return $app['twig']->render('formulaireContact.html.twig', array());
 })
 ->bind('formulaireContact')
@@ -124,12 +143,14 @@ $app->match('/formulaireAnnonce', function () use ($app) {
 ;
 //----------------------------------------------------------------------------------------------------
 $app->get('/AproposDeNous', function () use ($app) {
+
     return $app['twig']->render('parallax.html.twig', array());
 })
 ->bind('AproposDeNous')
 ;
 //----------------------------------------------------------------------------------------------------
 $app->error(function (\Exception $e, Request $request, $code) use ($app) {
+    
     if ($app['debug']) {
         return;
     }
