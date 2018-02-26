4a13b1cbb643d92d665bde39a791301892516703
Unknown <zephyrmahe@msn.com>
2018-02-26 10:17:58 +0100

    correction bug navbar

--- web/js/nav.js
+++ web/js/nav.js
@@ -1,10 +1,20 @@
+$(function() {
+    var windowWidth = $(window).width();
+    if(windowWidth <= 991){
+        $('.hr').show();
+    }
+    else {
+        $('.hr').hide();
+    }
+});
+
 function redimension() {
     var windowWidth = $(window).width();
     if(windowWidth <= 991){
-        $('hr').show();
+        $('.hr').show();
     }
     else {
-        $('hr').hide();
+        $('.hr').hide();
     }
 };
 window.addEventListener('resize', redimension, false);
